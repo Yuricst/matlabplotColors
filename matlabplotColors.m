0b9c86207054730c5789e7ffcd98a47e310c6f3c
Yuri <50186716+Yuricst@users.noreply.github.com>
2020-03-30 09:51:54 +0100

    Initial upload

---
+++ matlabplotColors.m
@@ -0,0 +1,86 @@
+%% Define additional colors for Matlab plotting
+% ================================================================ %
+% Method: 
+% 	call this script internally before using the plot() (or similar) function
+%	>> plot(<x>, <y>, 'color', <color_name>)
+% Eample:
+%	>> plot(0,0,'color',matlabB);
+%	
+% Available color names:
+%	MATLAB default colors (default color when no specification is provided)
+%		matlabB : default Matlab blue
+%		matlabR : default Matlab red
+%		matlabY : default Matlab yellow
+%		matlabM : default Matlab purple
+%		matlabG : default Matlab green
+%		matlabC : default Matlab cian
+%		matlabB : default Matlab brown
+%
+%	Google colors
+%		googleB : Google logo blue
+%		googleR : Google logo red
+%		googleY : Google logo yellow
+%		googleG : Google logo green
+%
+%	Japan theme colors
+%       japanB : navy blue / ai (?)
+%       japanY : yellow / yamabuki (??)
+%       japanG : beige / gofun (??)
+%       japanK : light brown / kuchiba (??)
+%       japanM : dark red / suo (??)
+%       japanP : purple / ayame (??)
+%       japanA : blue-grey / ainezu (??)
+%       japanE : brown-red / enji (??)
+%       japanT : dark blue / tetsukon (??)
+%	
+% 	Apple theme colors
+%       appleR : Apple iOS red
+%       appleB : Apple iOS blue
+%       appleO : Apple iOS orange
+%       appleY : Apple iOS yellow
+%       appleG : Apple iOS green
+%       appleC : Apple iOS teal blue (cyan)
+%       appleP : Apple iOS purple
+%       appleM : Apple iOS pink (magenta)
+% 
+% Yuri Shimane, 2020/03/27
+% ================================================================ %
+
+% ===== MATLAB DEFAULT COLORS ===== %
+matlabB = '#0072BD';  % blue
+matlabR = '#D95319';  % red
+matlabY = '#EDB120';  % yellow (orange)
+matlabP = '#7E2F8E';  % purple
+matlabG = '#77AC30';  % green
+matkabC = '#4DBEEE';  % cyan (light blue)
+matlabM = '#A2142F';  % maroon
+
+% ===== JAPAN COLORS ===== %
+% https://mag.japaaan.com/archives/8397
+japanB = '#284a6a';  % ai (?)
+japanY = '#e9ab14';  % yamabuki (??)
+japanG = '#ebe8dc';  % gofun (??)
+japanK = '#847968';  % kuchiba (??)
+japanM = '#ab4031';  % suo (??)
+japanP = '#644b7a';  % ayame (??)
+japanA = '#516a7a';  % ainezu (??)
+japanE = '#ab4031';  % enji (??)
+japanT = '#242830';  % tetsukon (??)
+
+% ===== GOOGLE COLORS ===== %
+googleB = '#4285f4';  % Google logo blue
+googleR = '#ea4335';  % Google logo red
+googleY = '#fbbc05';  % Google logo yellow
+googleG = '#34a853';  % Google logo green
+
+% === APPLE COLORS ===== %
+appleR = '#ff3b30';  % Apple iOS red
+appleB = '#007aff';  % Apple iOS blue
+appleO = '#ff9500';  % Apple iOS orange
+appleY = '#ffcc00';  % Apple iOS yellow
+appleG = '#4cd964';  % Apple iOS green
+appleC = '#5ac8fa';  % Apple iOS teal blue (cyan)
+appleP = '#5856d6';  % Apple iOS purple
+appleM = '#ff2d55';  % Apple iOS pink (magenta)
+
+
